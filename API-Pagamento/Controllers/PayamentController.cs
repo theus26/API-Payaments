6cf9ddee839ad597cae23cae0ac5e78bf310b4f6
Matheus Filipe <theuslipe26@gmail.com>
2023-07-30 00:55:37 -0300

    Feat: adicionado Controller

---
+++ API-Pagamento/Controllers/PayamentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Pagamento.Controllers
+{
+    public class PayamentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
